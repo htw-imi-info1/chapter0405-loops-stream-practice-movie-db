ce6e0030299c87f116b120960daff3bdfeb0ebad
B Kleinen <kleinen@infrastructure.de>
2019-11-12 14:11:27 +0100

    Ex 2) add title field to movie

--- Example/Movie.java
+++ Example/Movie.java
@@ -2,11 +2,18 @@
 public class Movie
 {
     private int releaseYear = 0;
+    private String title; 
 
-    public Movie(int releaseYear){
+    public Movie(int releaseYear, String title){
         this.releaseYear = releaseYear;
+        this.title = title;
     }
+
     public int getReleaseYear(){return releaseYear;}
+
+    public String getTitle(){
+        return title;
+    }
 
     public String toString(){
         return "I'm an Movie object and my fields are: ...."+releaseYear; 
