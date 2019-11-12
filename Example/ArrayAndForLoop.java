3a6265e2c7621f4013febf420f63a149383a4ebb
B Kleinen <kleinen@infrastructure.de>
2019-11-12 14:07:57 +0100

    Ex 1) print out all the numbers

--- Example/ArrayAndForLoop.java
+++ Example/ArrayAndForLoop.java
@@ -6,6 +6,8 @@ public class ArrayAndForLoop
     
     {
         int[] numbers = { 4, 1, 22, 9, 14, 3, 9};
-        //for...
+        for(int i=0; i < numbers.length; i++){
+        System.out.println("numbers["+i+ "] : "+ numbers[i]);
+        }
     }
 }
