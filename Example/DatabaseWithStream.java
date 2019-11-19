38aed752f873de9eaa0e4a0b9c921c9c9f5fec8c
B Kleinen <kleinen@infrastructure.de>
2019-11-19 14:22:39 +0100

    Ex 15) how long would it take to watch all movies     - in a given year     - from a certain director

--- Example/DatabaseWithStream.java
+++ Example/DatabaseWithStream.java
@@ -19,15 +19,24 @@ public class DatabaseWithStream
             .collect(Collectors.joining("\n")));
     }
 
+    // Ex 13) Print all Movies with a number using a for loop.
+    public void printAllWithNumbers(){
+
+    }
+
     private void generateSomeTestData(){
         // if you have a constructor accepting all your 
         // fields, it's easy to create the test data
         // in one row like this:
-        collection.add(new Movie(1950,"The Godfather"));
-        collection.add(new Movie(1951,"Lord of the rings"));
-        collection.add(new Movie(1950,"Avengers"));
-        collection.add(new Movie(1951,"Matrix"));
-        collection.add(new Movie(1952,"Pulp Fiction"));
+        collection.add(new Movie(1972,"The Godfather","Francis Ford Coppola"));
+        collection.add(new Movie(2001,"Lord of the rings","Peter Jackson"));
+        collection.add(new Movie(2012,"Avengers","Joss Whedon"));
+        collection.add(new Movie(2019,"Film without director","",90));
+        collection.add(new Movie(2019,"Another Film in 2019","",55));
+        collection.add(new Movie(2019,"Third Film in 2019","",80));
+        collection.add(new Movie(1999,"The Matrix","Lana Wachowski"));
+        collection.add(new Movie(1994,"Pulp Fiction","Quentin Tarantino",80));
+        collection.add(new Movie(1995,"Pulp Fiction II","Quentin Tarantino",80));
     }
 
     public void printWatchedInYear(int year){
@@ -50,9 +59,25 @@ public class DatabaseWithStream
         .removeIf(movie -> movie.getReleaseYear() == year);
     }
 
-
     public void demo1(){
         System.out.println(getWatchedInYear(1950));
     }
 
+    /*
+    Ex 15) how long would it take to watch all movies
+    - in a given year
+    - from a certain director
+     */
+    public int getDurationForYear(int year){
+        return collection.stream()
+        .filter(movie -> movie.getReleaseYear() == year)
+        .map(movie -> movie.getDuration())
+        .reduce(0, (sum, duration) -> sum + duration);
+    }
+    public int getDurationForDirector(String director){
+        return collection.stream()
+        .filter(movie -> director.equals(movie.getDirector()))
+        .map(movie -> movie.getDuration())
+        .reduce(0, (sum, duration) -> sum + duration);
+    }
 }
